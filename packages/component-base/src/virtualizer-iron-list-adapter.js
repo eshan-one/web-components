46645d2dde84ec52ea3ba06afa191b1a70988219
Tomi Virkki <virkki@vaadin.com>
2023-10-05 16:32:35 +0300

    fix: visually hide virtualizer placeholders

--- packages/component-base/src/virtualizer-iron-list-adapter.js
+++ packages/component-base/src/virtualizer-iron-list-adapter.js
@@ -221,6 +221,7 @@ export class IronListAdapter {
     // Clean up temporary placeholder sizing
     if (el.__virtualizerPlaceholder) {
       el.style.paddingTop = '';
+      el.style.visibility = '';
       el.__virtualizerPlaceholder = false;
     }
 
@@ -245,6 +246,7 @@ export class IronListAdapter {
         // Assign a temporary placeholder sizing to elements that would otherwise end up having
         // no height.
         el.style.paddingTop = `${this.__placeholderHeight}px`;
+        el.style.visibility = 'hidden';
         el.__virtualizerPlaceholder = true;
 
         // Manually schedule the resize handler to make sure the placeholder padding is
